eb4756b16bd2e881bb465964006a84320e166422
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-05-09 10:53:37 -0400

    se agregan dto

--- src/main/java/cl/app/ms_products_db/service/ProductService.java
+++ src/main/java/cl/app/ms_products_db/service/ProductService.java
@@ -1,10 +1,12 @@
 package cl.app.ms_products_db.service;
 
+import cl.app.ms_products_db.model.dto.ProductDTO;
 import cl.app.ms_products_db.model.entities.Product;
 import cl.app.ms_products_db.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +15,20 @@ public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
-    public List<Product> findAll(){
-        return productRepository.findAll();
+    public List<ProductDTO> findAll(){
+        List<ProductDTO> dtoList = new ArrayList<>();
+
+        for(Product product: productRepository.findAll()){
+            dtoList.add(entityToDto(product));
+        }
+        return dtoList;
+    }
+
+    public ProductDTO entityToDto(Product product){
+        return new ProductDTO(product.getId(),
+                              product.getName(),
+                              product.getDescription(),
+                              product.getPrice());
     }
 
 }
